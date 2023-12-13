44742891c5ca8156e45b8f9db3c14aa6d40ee09e
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-12-13 16:59:29 +0300

    UPD: added links

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(options={})
 browser.get('https://www.copart.com/todaysAuction')
 browser.implicitly_wait(5)
 links = browser.find_elements(By.LINK_TEXT, "View List")
 
-print(links)
+for link in links:
+  print(link.get_attribute("href"))
